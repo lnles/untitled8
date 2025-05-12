49467900d22ff524e71ff681485a117e04733aea
lnles <lnles@aol.com>
2025-05-13 03:21:05 +0800

    try: dynamic_background

--- lib/features/home/likes_screen.dart
+++ lib/features/home/likes_screen.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:dynamic_background/domain/enums/scroll_direction.dart';
+import 'package:dynamic_background/domain/enums/scroller_shape.dart';
+import 'package:dynamic_background/domain/models/painter_data/scroller_painter_data.dart';
+import 'package:dynamic_background/widgets/views/dynamic_bg.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:ionicons/ionicons.dart';
-import 'package:untitled8/app_router.gr.dart';
 
 @RoutePage()
 class LikesScreen extends HookWidget {
@@ -10,12 +14,11 @@ class LikesScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    var counter = useState(0);
-    void incrementCounter() => counter.value++;
-    void toDetail () {
-      incrementCounter();
-      context.router.push(DetailRoute(id: 1212));
-    }
+
+    final double buttonWidth = 500.0;
+    final double buttonHeight = 65.0;
+    final BorderRadius buttonRadius = BorderRadius.circular(5.0);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -23,21 +26,72 @@ class LikesScreen extends HookWidget {
       ),
       backgroundColor: Theme.of(context).colorScheme.surface,
       // backgroundColor: Colors.white.withOpacity(0.9),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('You have pushed the button this many times:'),
-            Text(
-              '${counter.value}',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: ClipRRect(
+        borderRadius: buttonRadius,
+        child: DynamicBg(
+          width: buttonWidth,
+          height: buttonHeight,
+          /*painterData: LavaPainterData(
+            width: 250.0,
+            widthTolerance: 75.0,
+            growAndShrink: true,
+            growthRate: 10.0,
+            growthRateTolerance: 5.0,
+            blurLevel: 25.0,
+            numBlobs: 5,
+            backgroundColor: ColorSchemes.gentleWhiteBg,
+            colors: [
+              ColorSchemes.vibrantOrangeBg,
+              ColorSchemes.vibrantOrangeFg,
+              ColorSchemes.vibrantYellowBg,
+              ColorSchemes.vibrantYellowFg,
+            ],
+            allSameColor: false,
+            fadeBetweenColors: true,
+            changeColorsTogether: false,
+            speed: 20.0,
+            speedTolerance: 5.0,
+          ),*/
+          /*painterData: WavePainterData(
+            waves: [
+              Wave(
+                direction: WaveDirection.left2Right,
+                amplitude: 50.0,
+                offset: 0.5,
+                color: Colors.transparent,
+                lineColor: ColorSchemes.gentleBlueBg,
+                lineThickness: 3.0,
+              ),
+            ],
+          ),*/
+          painterData: ScrollerPainterData(
+            direction: ScrollDirection.right2Left,
+            shape: ScrollerShape.stripesDiagonalForward,
+            backgroundColor: Color.fromARGB(255, 214, 232, 185),
+            color: Color.fromARGB(255, 198, 225, 151),
+            shapeWidth: 24.0,
+            spaceBetweenShapes: 24.0,
+            fadeEdges: false,
+          ),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {
+                log('Button pressed');
+              },
+              borderRadius: buttonRadius,
+              child: Center(
+                child: Text(
+                  'Save',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
             ),
-            TextButton.icon(
-              onPressed: toDetail,
-              icon: Icon(Ionicons.home),
-              label: Text('Go to Detail Screen'),
-            )
-          ],
+          ),
         ),
       ),
     );
